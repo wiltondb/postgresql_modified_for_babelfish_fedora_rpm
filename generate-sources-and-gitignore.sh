06a51ffa8aa13f3136745d2cf009340fd3b5c30e
Pavel Raiskup <praiskup@redhat.com>
2016-10-26 09:54:31 +0200

    gitignore: add up2date items
    And fix related script.
    
    Version: 9.6.1-1

--- generate-sources-and-gitignore.sh
+++ generate-sources-and-gitignore.sh
@@ -1,10 +1,13 @@
-#! /bin/sh -x
+#! /bin/sh
 
+rm sources .gitignore
 set -e
 spectool -S *.spec | cut -d' ' -f2 \
     | grep -E -e 'postgresql-.*\.tar\.*' -e 'postgresql.*\.pdf' | sort | \
 while read line
 do
     base=`basename "$line"`
-    md5sum "$base"
+    echo " * handling $base"
+    md5sum "$base" >> sources
+    echo "/$base" >> .gitignore
 done
