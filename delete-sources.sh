b254acaea93f38bd5c5b04af416e6bfc403c6314
alex <alex@staticlibs.net>
2022-12-17 21:25:23 +0000

    change sources handling for copr

---
+++ delete-sources.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+set -x
+
+rm ./BABEL_2_2_0__PG_14_5.tar.gz
+touch ./BABEL_2_2_0__PG_14_5.tar.gz
+rm ./postgresql-13.7.tar.bz2
+touch ./postgresql-13.7.tar.bz2
+rm ./postgresql-14.5-US.pdf
+touch ./postgresql-14.5-US.pdf
+rm ./postgresql-setup-8.7.tar.gz
+touch ./postgresql-setup-8.7.tar.gz 
