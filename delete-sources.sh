f825c9074ad3049f1f571d4d9393f520e33ebf49
alex <alex@staticlibs.net>
2023-06-30 14:39:43 +0100

    update to BABEL_3_2_STABLE 9c4b781

--- delete-sources.sh
+++ delete-sources.sh
@@ -3,9 +3,8 @@
 set -e
 set -x
 
-VERSION_BABEL=3_1
-VERSION_PG=15_2
-VERSION_POSTRESQL=15.2
+VERSION_BABEL=3_2
+VERSION_PG=15_3
 VERSION_POSTRESQL_PREV=14.3
 VERSION_POSTRESQL_SETUP=8.8
 
@@ -13,7 +12,5 @@ rm -f ./BABEL_${VERSION_BABEL}__PG_${VERSION_PG}.tar.gz
 touch ./BABEL_${VERSION_BABEL}__PG_${VERSION_PG}.tar.gz
 rm -f ./postgresql-${VERSION_POSTRESQL_PREV}.tar.bz2
 touch ./postgresql-${VERSION_POSTRESQL_PREV}.tar.bz2
-rm -f ./postgresql-${VERSION_POSTRESQL}-US.pdf
-touch ./postgresql-${VERSION_POSTRESQL}-US.pdf
 rm -f ./postgresql-setup-${VERSION_POSTRESQL_SETUP}.tar.gz
 touch ./postgresql-setup-${VERSION_POSTRESQL_SETUP}.tar.gz 
